2487c9e4e003401461987e618e07899109352904
hLachev <icoicic@abv.bg>
2015-06-17 18:40:46 +0300

    Update Moving.cpp

--- Project/Moving.cpp
+++ Project/Moving.cpp
@@ -1,11 +1,28 @@
 #include"Moving.h"
 
-class Physics
+const double dist = 31;
+const int LowerBoarder = 0;
+const int UpperBoarder = 150;
+const int MoveRate = 30;
+
+bool Moving::Isclose(const Entity& x1,const Entity& y2)
 {
-public:
+	return  x1.GetPosition().GetDistance(y2.GetPosition()) < dist;
+}
 
-	bool Isclose(const Entity& ent1,const Entity& ent2);
-	void Move(Entity& ent);
+void Moving::Move(Entity& var)
+{
+	double dx = RandomGenerator::GetRGen()->RandomNumber(MoveRate);
+	double dy = RandomGenerator::GetRGen()->RandomNumber(MoveRate);
 
-
-};
+	if( var.GetPosition().GetX()+dx < LowerBoarder || var.GetPosition().GetX()+dx >UpperBoarder  )
+	{
+		dx = 0;
+	}
+	if( var.GetPosition().GetY()+dy < LowerBoarder || var.GetPosition().GetY()+dy >UpperBoarder)
+	{
+		dy = 0;
+	}
+	
+	var.Move(Point2D(dx,dy));
+}
