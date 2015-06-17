384ab1e49d9b2490692d469a97d67f412348bf8c
hLachev <icoicic@abv.bg>
2015-06-17 18:41:53 +0300

    Create Planet.h

---
+++ Project/Planet.h
@@ -0,0 +1,29 @@
+#include<vector>
+#include"Entity.h"
+#include<map>
+#include<functional>
+#include<algorithm>
+
+using namespace std::placeholders;
+
+#pragma once
+
+class Planet: public Entity
+{
+public:
+
+	Planet();
+
+	void SortEntities();
+
+	bool EntitesComparator(const std::unique_ptr<Entity>& ent1,const std::unique_ptr<Entity>& ent2);
+
+	std::vector<std::unique_ptr<Entity>>& GetEntities();
+
+	std::map<EntityType, int>& GetEntityAdded();
+
+private:
+
+	std::vector<std::unique_ptr<Entity>> m_vEntities;
+	std::map<EntityType, int> m_EntitiesToBeAdded;
+};
